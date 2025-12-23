e1fc451866ccd3b8e369784d61589f9e99a781d8
Vagish Reddy <gvagishr@gmail.com>
2025-12-23 16:49:15 +0530

    Add files via upload

---
+++ mathModule.py
@@ -0,0 +1,11 @@
+import math
+
+num = float(input("Enter a number: "))
+
+squareroot = math.sqrt(num)
+naturallog = math.log(num)
+sinevalue = math.sin(num)
+
+print("Square Root of", num, "is:", squareroot)
+print("Natural Logarithm of", num, "is:", naturallog)
+print("Sin of", num, "is:", sinevalue)
